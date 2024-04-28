4b95b3f69934bf758fd82e649fb53e84abecb636
vonot16 <rafaelfeliperm@gmail.com>
2024-04-28 20:07:33 -0300

    User Model Change to use Cookie Auth

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from djongo import models
 from django import forms
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -16,8 +17,12 @@ class MessageForm(forms.ModelForm):
         model = Message
         fields = {'message', 'isUserMessage'}
 
-class User(models.Model):
+class User(AbstractUser):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True, max_length=254)
     password = models.CharField(max_length=100)
     messages = models.ArrayField(model_container=Message, model_form_class=MessageForm)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
