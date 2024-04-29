2c65ef279367846f00e9fab659c1231a0d1a6bdc
vonot16 <rafaelfeliperm@gmail.com>
2024-04-28 21:16:08 -0300

    Login Function

--- api/serializers.py
+++ api/serializers.py
@@ -11,6 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'password', 'messages']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.response import Response
 
 from api.models import User, Message
 from api.serializers import UserSerializer, MessageSerializer
@@ -36,3 +37,22 @@ def register(request):
             user_serializer.save()
             return JsonResponse(user_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def login(request):
+    if request.method == 'POST':
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None or not user.check_password(password):
+            return Response({
+                'message': 'Check your email or password'
+            })
+
+        return Response({
+            'message': 'User logged in successfully',
+            'email': email
+        })
